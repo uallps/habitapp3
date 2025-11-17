d7af81d1c4947941655ac0eefed078750b14f57e
Ismael Fernández Méndez <ifm562@inlumine.ual.es>
2025-11-17 20:09:45 +0100

    Uploading updated comments: Understanding basic Swift.

--- HabitApp/Application/HabitApp.swift
+++ HabitApp/Application/HabitApp.swift
@@ -19,10 +19,14 @@ struct HabitApp: App {
 
     // Some scene is Swift's opaque type feature. It's basically: “body will return a specific type that conforms to Scene, but I don’t want to expose the concrete type here."
     var body: some Scene {
+        // WindowGroup is a struct in Swift (think of a Kotlin data class) that conforms to the Scene protocol. 
+        // You are initializing that struct. Swift allows trailing closure syntax, so if the last parameter is a function, 
+        // you can write it outside the parentheses.
         WindowGroup{
 #if os(iOS)
             TabView {
                 HabitListView()
+                // Dot for modifiers system (SwiftUI), DSL-Like
                     .tabItem {
                         Label("Hábitos", systemImage: "checklist")
                     }
