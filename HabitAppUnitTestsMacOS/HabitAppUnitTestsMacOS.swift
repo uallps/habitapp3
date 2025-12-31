70e359dc1fbd39953c2aad35b7c791d3d95aa889
Ismael Fernandez Mendez <ifm562@inlumine.ual.es>
2025-12-31 19:42:29 +0100

    fix(xcode-tests): fix iOS test target invoking a MacOS only build.
    As an attempt to fix #10, it's good practice to write an Unit Test about the sequence to be fixed.

---
+++ HabitAppUnitTestsMacOS/HabitAppUnitTestsMacOS.swift
@@ -0,0 +1,19 @@
+//
+//  HabitAppUnitTestsMacOS.swift
+//  HabitAppUnitTestsMacOS
+//
+//  Created by Aula03 on 31/12/25.
+//
+
+import Testing
+@testable import HabitApp
+
+struct HabitAppUnitTestsMacOS {
+
+    @Test func example() async throws {
+        val parentCategory = Category(
+            
+        )
+    }
+
+}
