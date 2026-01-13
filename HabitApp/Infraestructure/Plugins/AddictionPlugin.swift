87b46445803b06015eddc68048f4da77257eeaa1
Ismael Fernandez Mendez <ifm562@inlumine.ual.es>
2026-01-13 08:19:41 +0100

    fix: runtime storageProviderError
    Stop AddictionPlugin from owning storageProvider and reference it before it exists, make it own a variable referencing the singleton of AppConfig instead.

--- HabitApp/Infraestructure/Plugins/AddictionPlugin.swift
+++ HabitApp/Infraestructure/Plugins/AddictionPlugin.swift
@@ -5,21 +5,18 @@ final class AddictionPlugin: HabitDataObservingPlugin {
     
     func onDataChanged(taskId: UUID, title: String, dueDate: Date?) {
         Task {
-            try await storageProvider.onDataChanged(taskId: taskId, title: title, dueDate: dueDate)
+            try await config.storageProvider.onDataChanged(taskId: taskId, title: title, dueDate: dueDate)
         }
     }
     
-    let storageProvider: StorageProvider
-    
-    
     var models: [any PersistentModel.Type]
-    
+    let config: AppConfig
     var isEnabled: Bool
     
     init(config: AppConfig) {
         self.isEnabled = config.userPreferences.enableAddictions
-        self.models = [Habit.self, Addiction.self]
-        self.storageProvider = config.storageProvider
+        self.models = [Addiction.self]
+        self.config = config
     }
     
     
